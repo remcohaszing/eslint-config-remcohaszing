7188b5ab19084afa7f0647408d734ff0dbdbd9f4
Remco Haszing <remcohaszing@gmail.com>
2020-08-08 11:57:09 +0200

    Disable some rules for development
    `jsdoc/require-jsdoc` and `unicorn/consistent-function-scoping` are
    typically not desired in test files, configuration files, or
    documentation.

--- eslint-config/dev.js
+++ eslint-config/dev.js
@@ -17,8 +17,22 @@ module.exports = {
     ],
 
     /**
+     * Https://github.com/gajus/eslint-plugin-jsdoc/blob/master/.README/rules/require-jsdoc.md
+     *
+     * JSDoc is often not relevant in test or configuration files.
+     */
+    'jsdoc/require-jsdoc': 'off',
+
+    /**
      * https://github.com/mysticatea/eslint-plugin-node/blob/master/docs/rules/no-unpublished-require.md
      */
     'node/no-unpublished-require': 'off',
+
+    /**
+     * https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/consistent-function-scoping.md
+     *
+     * This is an optimization that’s not important for tests or confiurations.
+     */
+    'unicorn/consistent-function-scoping': 'off',
   },
 };
