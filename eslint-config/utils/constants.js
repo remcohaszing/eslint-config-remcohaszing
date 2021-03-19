1c4e4f05c7bedf57a20062f9aca64c1b95c00050
Remco Haszing <remcohaszing@gmail.com>
2021-03-19 17:50:21 +0100

    Add estree-jsx to dtOnlyPackages

--- eslint-config/utils/constants.js
+++ eslint-config/utils/constants.js
@@ -1,7 +1,15 @@
 /**
  * Known packages in on DefinitelyTyped that don’t describe an npm package.
  */
-module.exports.dtOnlyPackages = ['estree', 'hast', 'mdast', 'unist', 'web-app-manifest', 'xast'];
+module.exports.dtOnlyPackages = [
+  'estree',
+  'estree-jsx',
+  'hast',
+  'mdast',
+  'unist',
+  'web-app-manifest',
+  'xast',
+];
 
 /**
  * Known packages that export only type definitions, but which don’t contain a JavaScript file.
