ed86b158a576169b3dba4d082337bf99d0b3d221
Remco Haszing <remcohaszing@gmail.com>
2022-08-22 14:13:57 +0200

    Remove irrelevant rule overrides for web

--- web.js
+++ web.js
@@ -25,16 +25,6 @@ module.exports = {
     'n/no-unpublished-bin': 'off',
 
     /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/no-unpublished-import.md
-     */
-    'n/no-unpublished-import': 'error',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/no-unpublished-require.md
-     */
-    'n/no-unpublished-require': 'error',
-
-    /**
      * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features/es-builtins.md
      */
     'n/no-unsupported-features/es-builtins': 'off',
@@ -48,44 +38,5 @@ module.exports = {
      * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/no-unsupported-features/node-builtins.md
      */
     'n/no-unsupported-features/node-builtins': 'off',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/prefer-global/buffer.md
-     */
-    'n/prefer-global/buffer': 'off',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/prefer-global/text-decoder.md
-     *
-     * Re-enabled for TypeScript.
-     */
-    'n/prefer-global/text-decoder': 'error',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/prefer-global/text-encoder.md
-     *
-     * Re-enabled for TypeScript.
-     */
-    'n/prefer-global/text-encoder': 'error',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/prefer-promises/dns.md
-     */
-    'n/prefer-promises/dns': 'off',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/prefer-promises/fs.md
-     */
-    'n/prefer-promises/fs': 'off',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/process-exit-as-throw.md
-     */
-    'n/process-exit-as-throw': 'off',
-
-    /**
-     * https://github.com/weiran-zsd/eslint-plugin-node/blob/master/docs/rules/shebang.md
-     */
-    'n/shebang': 'off',
   },
 };
