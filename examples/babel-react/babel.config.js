810ce6bf8bda15a711204bc16a73fc6b7b580215
Remco Haszing <remcohaszing@gmail.com>
2023-04-23 15:24:57 +0200

    Switch examples to vite

--- examples/babel-react/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  presets: [['@babel/preset-react', { runtime: 'automatic' }]],
-  plugins: ['@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-class-properties']
-}
