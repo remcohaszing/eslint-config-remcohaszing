8caa1aeafbc92c39f7073309d0e5cebb90f3e617
Remco Haszing <remcohaszing@gmail.com>
2024-04-06 19:33:00 +0200

    Mark imports starting with # as internal for sorting

--- plugins/import.js
+++ plugins/import.js
@@ -270,6 +270,10 @@ module.exports = {
         alphabetize: { order: 'asc', caseInsensitive: true },
         groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index'], 'object'],
         'newlines-between': 'always',
+        pathGroups: [
+          { pattern: '#*', group: 'internal' },
+          { pattern: '#/**', group: 'internal' }
+        ],
         warnOnUnassignedImports: true
       }
     ],
