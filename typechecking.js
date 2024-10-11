5d41e37d5b93da70f369dac8c30301a3cf5168ae
Remco Haszing <remcohaszing@gmail.com>
2024-10-11 16:11:25 +0200

    Remove rule comments

--- typechecking.js
+++ typechecking.js
@@ -9,160 +9,37 @@ module.exports = {
         project: true
       },
       rules: {
-        /**
-         * https://typescript-eslint.io/rules/await-thenable
-         */
         '@typescript-eslint/await-thenable': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/consistent-return
-         */
         '@typescript-eslint/consistent-return': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/consistent-type-exports
-         */
         '@typescript-eslint/consistent-type-exports': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/dot-notation
-         */
         '@typescript-eslint/dot-notation': 'error',
         'dot-notation': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/no-array-delete"
-         */
         '@typescript-eslint/no-array-delete': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-confusing-void-expression"
-         */
         '@typescript-eslint/no-confusing-void-expression': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/no-duplicate-type-constituents
-         */
         '@typescript-eslint/no-duplicate-type-constituents': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-base-to-string
-         */
         '@typescript-eslint/no-base-to-string': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-floating-promises
-         */
         '@typescript-eslint/no-floating-promises': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/no-for-in-array
-         */
         '@typescript-eslint/no-for-in-array': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-implied-eval
-         */
         '@typescript-eslint/no-implied-eval': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-misused-promises
-         */
         '@typescript-eslint/no-misused-promises': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/no-meaningless-void-operator
-         */
         '@typescript-eslint/no-meaningless-void-operator': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-mixed-enums
-         */
         '@typescript-eslint/no-mixed-enums': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/no-redundant-type-constituents
-         */
         '@typescript-eslint/no-redundant-type-constituents': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unnecessary-boolean-literal-compare
-         */
         '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unnecessary-condition
-         */
         '@typescript-eslint/no-unnecessary-condition': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unnecessary-qualifier
-         */
         '@typescript-eslint/no-unnecessary-qualifier': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unnecessary-type-arguments
-         */
         '@typescript-eslint/no-unnecessary-type-arguments': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unnecessary-type-assertion
-         */
         '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-argument
-         */
         '@typescript-eslint/no-unsafe-argument': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-assignment
-         */
         '@typescript-eslint/no-unsafe-assignment': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-call
-         */
         '@typescript-eslint/no-unsafe-call': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-enum-comparison
-         */
         '@typescript-eslint/no-unsafe-enum-comparison': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-member-access
-         */
         '@typescript-eslint/no-unsafe-member-access': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-return
-         */
         '@typescript-eslint/no-unsafe-return': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-unsafe-unary-minus
-         */
         '@typescript-eslint/no-unsafe-unary-minus': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/no-useless-template-literals
-         */
         '@typescript-eslint/no-useless-template-literals': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/non-nullable-type-assertion-style
-         */
         '@typescript-eslint/non-nullable-type-assertion-style': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/only-throw-error
-         */
         '@typescript-eslint/only-throw-error': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-destructuring
-         */
         '@typescript-eslint/prefer-destructuring': [
           'error',
           {
@@ -171,117 +48,33 @@ module.exports = {
           }
         ],
         'prefer-destructuring': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-find
-         */
         '@typescript-eslint/prefer-find': 'error',
         'unicorn/prefer-array-find': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-includes
-         */
         '@typescript-eslint/prefer-includes': 'error',
         'unicorn/prefer-includes': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-nullish-coalescing
-         */
         '@typescript-eslint/prefer-nullish-coalescing': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-optional-chain
-         */
         '@typescript-eslint/prefer-optional-chain': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-promise-reject-errors
-         */
         '@typescript-eslint/prefer-promise-reject-errors': 'error',
         'prefer-promise-reject-errors': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-readonly
-         */
         '@typescript-eslint/prefer-readonly': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-readonly-parameter-types
-         */
         '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-reduce-type-parameter
-         */
         '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-regexp-exec
-         */
         '@typescript-eslint/prefer-regexp-exec': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-return-this-type
-         */
         '@typescript-eslint/prefer-return-this-type': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/prefer-string-starts-ends-with
-         */
         '@typescript-eslint/prefer-string-starts-ends-with': 'error',
         'unicorn/prefer-starts-ends-with': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/promise-function-async
-         */
         '@typescript-eslint/promise-function-async': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/require-array-sort-compare
-         */
         '@typescript-eslint/require-array-sort-compare': ['error', { ignoreStringArrays: true }],
-
-        /**
-         * https://typescript-eslint.io/rules/require-await
-         */
         '@typescript-eslint/require-await': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/restrict-plus-operands
-         */
         '@typescript-eslint/restrict-plus-operands': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/restrict-template-expressions
-         */
         '@typescript-eslint/restrict-template-expressions': [
           'error',
           { allowNumber: true, allowBoolean: true, allowAny: false, allowNullish: true }
         ],
-
-        /**
-         * https://typescript-eslint.io/rules/return-await
-         */
         '@typescript-eslint/return-await': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/strict-boolean-expressions
-         */
         '@typescript-eslint/strict-boolean-expressions': 'off',
-
-        /**
-         * https://typescript-eslint.io/rules/switch-exhaustiveness-check
-         */
         '@typescript-eslint/switch-exhaustiveness-check': 'error',
-
-        /**
-         * https://typescript-eslint.io/rules/unbound-method
-         */
         '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
-
-        /**
-         * https://typescript-eslint.io/rules/use-unknown-in-catch-callback-variable
-         */
         '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error'
       }
     }
--- typescript.js
+++ typescript.js
@@ -10,140 +10,27 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    /**
-     * https://eslint.org/docs/latest/rules/array-callback-return
-     *
-     * Use TypeScript to determine this.
-     */
     'array-callback-return': 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/camelcase
-     *
-     * `@typescript-eslint/naming-convention` is used instead.
-     */
     camelcase: 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/getter-return
-     */
     'getter-return': 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/new-cap
-     *
-     * Decorators should be allowed.
-     */
     'new-cap': 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/no-obj-calls
-     */
     'no-obj-calls': 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/no-setter-return
-     */
     'no-setter-return': 'off',
-
-    /**
-     * https://eslint.org/docs/latest/rules/no-undef
-     */
     'no-undef': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/default
-     *
-     * Use TypeScript to determine this.
-     */
     'import-x/default': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/no-extraneous-dependencies
-     *
-     * Handled by node/no-extraneous-import
-     *
-     * Use your own judgement to determine if this is a dependency or devDependency.
-     */
     'import-x/no-extraneous-dependencies:': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/named
-     *
-     * Use TypeScript to determine this.
-     */
     'import-x/named': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/namespace
-     *
-     * Use TypeScript to determine this.
-     */
     'import-x/namespace': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/no-deprecated
-     *
-     * This is triggered if function one call signature is deprecated, but not another.
-     */
     'import-x/no-deprecated': 'off',
-
-    /**
-     * https://github.com/benmosher/eslint-plugin-import-x/blob/main/docs/rules/no-named-as-default-member
-     *
-     * Use TypeScript to determine this.
-     */
     'import-x/no-named-as-default-member': 'off',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/no-types
-     */
     'jsdoc/no-types': 'error',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-param-type
-     */
     'jsdoc/require-param-type': 'off',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-property-type
-     */
     'jsdoc/require-property-type': 'off',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/require-returns-type
-     */
     'jsdoc/require-returns-type': 'off',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/no-undefined-types
-     */
     'jsdoc/no-undefined-types': 'off',
-
-    /**
-     * https://github.com/gajus/eslint-plugin-jsdoc/blob/main/.README/rules/valid-types
-     */
     'jsdoc/valid-types': 'off',
-
-    /**
-     * https://github.com/eslint-community/eslint-plugin-n/blob/main/docs/rules/no-unsupported-features/es-syntax
-     *
-     * TypeScript will compile this.
-     */
     'n/no-unsupported-features/es-syntax': 'off',
-
-    /**
-     * https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-json-parse-buffer
-     *
-     * The types don’t allow this.
-     */
     'unicorn/prefer-json-parse-buffer': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/naming-convention
-     *
-     * This only requires type checking for a very specific unused option.
-     */
     '@typescript-eslint/naming-convention': [
       'error',
       {
@@ -190,20 +77,8 @@ module.exports = {
       // TypeLike
       { selector: 'typeLike', format: ['PascalCase'] }
     ],
-
-    /**
-     * https://typescript-eslint.io/rules/adjacent-overload-signatures
-     */
     '@typescript-eslint/adjacent-overload-signatures': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/array-type
-     */
     '@typescript-eslint/array-type': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/ban-ts-comment
-     */
     '@typescript-eslint/ban-ts-comment': [
       'error',
       {
@@ -213,94 +88,28 @@ module.exports = {
         'ts-check': true
       }
     ],
-
-    /**
-     * https://typescript-eslint.io/rules/ban-tslint-comment
-     */
     '@typescript-eslint/ban-tslint-comment': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/ban-types
-     */
     '@typescript-eslint/ban-types': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/class-literal-property-style
-     */
     '@typescript-eslint/class-literal-property-style': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/class-methods-use-this
-     */
     '@typescript-eslint/class-methods-use-this': 'error',
     'class-methods-use-this': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/consistent-generic-constructors
-     */
     '@typescript-eslint/consistent-generic-constructors': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/consistent-indexed-object-style
-     */
     '@typescript-eslint/consistent-indexed-object-style': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/consistent-type-assertions
-     */
     '@typescript-eslint/consistent-type-assertions': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/consistent-type-definitions
-     */
     '@typescript-eslint/consistent-type-definitions': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/consistent-type-imports
-     */
     '@typescript-eslint/consistent-type-imports': [
       'error',
       { disallowTypeAnnotations: false, fixStyle: 'inline-type-imports' }
     ],
-
-    /**
-     * https://typescript-eslint.io/rules/default-param-last
-     */
     '@typescript-eslint/default-param-last': 'error',
     'default-param-last': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/explicit-function-return-type
-     */
     '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
-
-    /**
-     * https://typescript-eslint.io/rules/explicit-member-accessibility
-     */
     '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
-
-    /**
-     * https://typescript-eslint.io/rules/explicit-module-boundary-types
-     *
-     * Handled by @typescript-eslint/explicit-function-return-type
-     */
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/init-declarations
-     */
     '@typescript-eslint/init-declarations': 'off',
     'init-declarations': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/max-params
-     */
     '@typescript-eslint/max-params': 'off',
     'max-params': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/member-ordering
-     */
     '@typescript-eslint/member-ordering': [
       'error',
       {
@@ -346,147 +155,39 @@ module.exports = {
         ]
       }
     ],
-
-    /**
-     * https://typescript-eslint.io/rules/method-signature-style
-     */
     '@typescript-eslint/method-signature-style': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-array-constructor
-     */
     '@typescript-eslint/no-array-constructor': 'error',
     'no-array-constructor': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-confusing-non-null-assertion
-     */
     '@typescript-eslint/no-confusing-non-null-assertion': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-dupe-class-members
-     */
     '@typescript-eslint/no-dupe-class-members': 'error',
     'no-dupe-class-members': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-duplicate-enum-values
-     */
     '@typescript-eslint/no-duplicate-enum-values': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-dynamic-delete
-     */
     '@typescript-eslint/no-dynamic-delete': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-empty-function
-     */
     '@typescript-eslint/no-empty-function': 'error',
     'no-empty-function': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-empty-interface
-     */
     '@typescript-eslint/no-empty-interface': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-explicit-any
-     */
     '@typescript-eslint/no-explicit-any': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-extra-non-null-assertion
-     */
     '@typescript-eslint/no-extra-non-null-assertion': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-extraneous-class
-     */
     '@typescript-eslint/no-extraneous-class': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-import-type-side-effects
-     */
     '@typescript-eslint/no-import-type-side-effects': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-inferrable-types
-     */
     '@typescript-eslint/no-inferrable-types': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-invalid-this
-     */
     '@typescript-eslint/no-invalid-this': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-invalid-void-type
-     */
     '@typescript-eslint/no-invalid-void-type': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-loop-func
-     */
     '@typescript-eslint/no-loop-func': 'error',
     'no-loop-func': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-loss-of-precision
-     */
     '@typescript-eslint/no-loss-of-precision': 'error',
     'no-loss-of-precision': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-magic-numbers
-     */
     '@typescript-eslint/no-magic-numbers': 'off',
     'no-magic-numbers': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-misused-new
-     */
     '@typescript-eslint/no-misused-new': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-namespace
-     */
     '@typescript-eslint/no-namespace': ['error', { allowDefinitionFiles: true }],
-
-    /**
-     * https://typescript-eslint.io/rules/no-non-null-asserted-nullish-coalescing
-     */
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-non-null-asserted-optional-chain
-     */
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-non-null-assertion
-     */
     '@typescript-eslint/no-non-null-assertion': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-redeclare
-     */
     '@typescript-eslint/no-redeclare': ['error', { ignoreDeclarationMerge: false }],
     'no-redeclare': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-require-imports
-     */
     '@typescript-eslint/no-require-imports': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-restricted-imports
-     */
     '@typescript-eslint/no-restricted-imports': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-shadow
-     */
     '@typescript-eslint/no-shadow': [
       'error',
       {
@@ -496,123 +197,30 @@ module.exports = {
       }
     ],
     'no-shadow': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-this-alias
-     */
     '@typescript-eslint/no-this-alias': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-unnecessary-type-constraint
-     */
     '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-unsafe-declaration-merging
-     *
-     * Handled by `@typescript-eslint/no-redeclare`.
-     */
     '@typescript-eslint/no-unsafe-declaration-merging': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-unused-expressions
-     */
     '@typescript-eslint/no-unused-expressions': ['error', { enforceForJSX: true }],
     'no-unused-expressions': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-unused-vars
-     */
     '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'no-unused-vars': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-use-before-define
-     */
     '@typescript-eslint/no-use-before-define': 'error',
     'no-use-before-define': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-useless-constructor
-     *
-     * Don’t allow private constructors
-     */
     '@typescript-eslint/no-useless-constructor': 'off',
     'no-useless-constructor': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/no-useless-empty-export
-     */
     '@typescript-eslint/no-useless-empty-export': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/no-var-requires
-     */
     '@typescript-eslint/no-var-requires': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/parameter-properties
-     */
     '@typescript-eslint/parameter-properties': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-as-const
-     */
     '@typescript-eslint/prefer-as-const': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-enum-initializers
-     */
     '@typescript-eslint/prefer-enum-initializers': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-for-of
-     *
-     * Handled by unicorn/no-for-loop
-     */
     '@typescript-eslint/prefer-for-of': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-function-type
-     */
     '@typescript-eslint/prefer-function-type': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-literal-enum-member
-     */
     '@typescript-eslint/prefer-literal-enum-member': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-namespace-keyword
-     *
-     * Modules and namespaces are both disallowed, but for definition files namespaces are preferred
-     * over modules.
-     */
     '@typescript-eslint/prefer-namespace-keyword': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/prefer-ts-expect-error
-     */
     '@typescript-eslint/prefer-ts-expect-error': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/sort-type-union-constituents
-     */
     '@typescript-eslint/sort-type-constituents': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/triple-slash-reference
-     */
     '@typescript-eslint/triple-slash-reference': 'error',
-
-    /**
-     * https://typescript-eslint.io/rules/typedef
-     */
     '@typescript-eslint/typedef': 'off',
-
-    /**
-     * https://typescript-eslint.io/rules/unified-signatures
-     */
     '@typescript-eslint/unified-signatures': 'error'
   }
 }
